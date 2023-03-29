3a8aa32d6cff713d927278800d7c4c60633aa945
kimjinho1 <rlawlsgh8113@naver.com>
2023-03-30 01:34:53 +0900

    FEAT: Make user entity

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  // PrimaryGeneratedColumn -> 유니크한 id 자동 생성
+  // increment: id 1씩 증가, uuid: 유니크한 uuid 사용
+  // increment가 default인 듯함
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userId: string;
+
+  @Column()
+  Password: string;
+
+  @Column()
+  userName: string;
+}
