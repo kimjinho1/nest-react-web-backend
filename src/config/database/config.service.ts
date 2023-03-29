03d70a8d568edb07734ada8fa120dce0f0372932
kimjinho1 <rlawlsgh8113@naver.com>
2023-03-29 23:51:22 +0900

    INIT: TypeOrm SnakeNamingStrategy

--- src/config/database/config.service.ts
+++ src/config/database/config.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 @Injectable()
 export class PostgresConfigService implements TypeOrmOptionsFactory {
@@ -15,6 +16,9 @@ export class PostgresConfigService implements TypeOrmOptionsFactory {
       password: this.configService.get<string>('DB_PASSWORD'),
       database: this.configService.get<string>('DB_SCHEMA'),
       entities: ['dist/**/**/*.entity{.ts,.js}'],
+      // DB는 Snake case, ts는 Camel case를 사용하기 때문에 db key 이름들이 매칭이 안됨
+      // SnakeNamingStrategy(typeorm-naming-strategies)를 사용하면 알아서 Camel을 Snake case 형식으로 바꿔줌
+      namingStrategy: new SnakeNamingStrategy(),
       // synchronize: true,
       // autoLoadEntities: true
     };
