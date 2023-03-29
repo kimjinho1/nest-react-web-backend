e872a15a1a225c98a7855a5deec988bbf388b3a4
kimjinho1 <rlawlsgh8113@naver.com>
2023-03-30 01:35:13 +0900

    FEAT: Set ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // cors 문제 해결 -> api 주소 신뢰성 문제? -> 프록시 설정으로도 해결 가능한 듯함
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(3000);
   console.log('http://localhost:3000');
 }
