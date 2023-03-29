f5bbbd62fe3ea6cb78489ffaedecb012b74f571a
kimjinho1 <rlawlsgh8113@naver.com>
2023-03-29 23:34:46 +0900

    INIT: TypeOrm & .env Config Setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigModule } from './config/database/config.module';
+import { PostgresConfigService } from './config/database/config.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'jinho',
-      password: 'dlfhgk6262',
-      database: 'jinho',
-      entities: [],
-      synchronize: true,
+    // .env 설정 -> nestjs/config
+    ConfigModule.forRoot({ isGlobal: true }),
+    // typeorm Postgre DB 설정 -> nestjs/typeorm, pg
+    TypeOrmModule.forRootAsync({
+      imports: [PostgresConfigModule],
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
     }),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // cors 문제 해결 -> api 주소 신뢰성 문제? -> 프록시 설정으로도 해결 가능한 듯함
   app.enableCors();
   await app.listen(3000);
+  console.log('http://localhost:3000');
 }
 bootstrap();
