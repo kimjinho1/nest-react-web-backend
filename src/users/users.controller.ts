d45d2ee320ec63b493043c16508c80096e574616
kimjinho1 <rlawlsgh8113@naver.com>
2023-03-30 14:14:04 +0900

    FEAT: Public 데코레이터를 적용해 Guard를 벗김

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { Public } from 'src/auth/auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entity/user.entity';
@@ -16,27 +17,32 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Public()
   @Get()
   async getAllUser(): Promise<User[]> {
     return await this.usersService.getAllUser();
   }
 
+  @Public()
   @Get('/:id')
   async getUserById(@Param('id') userId: number): Promise<User> {
     return await this.usersService.getUserById(userId);
   }
 
+  @Public()
   @Post()
   async signUp(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.createUser(createUserDto);
   }
 
+  @Public()
   @Delete('/:id')
   async deleteUser(@Param('id') userId: number): Promise<void> {
     // return을 안하면 insomnia에서 에러가 안오는 것 같다
     return this.usersService.deleteUser(userId);
   }
 
+  @Public()
   @Patch('/:id')
   async updateUser(
     @Param('id') userId: number,
