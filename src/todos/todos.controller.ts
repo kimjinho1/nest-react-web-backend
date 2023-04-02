85e5128732dcdc77ba0d156507099e216b9a27bd
kimjinho1 <rlawlsgh8113@naver.com>
2023-04-03 02:22:11 +0900

    FEAT: Get all todos

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
+import { Public } from 'src/auth/auth.guard';
+import { UsersService } from 'src/users/users.service';
+import { TodosService } from './todos.service';
 
 @Controller('todos')
-export class TodosController {}
+export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
+  @Get()
+  async getAllTodos(@Request() req): Promise<string[]> {
+    return this.todosService.getAllTodos(req.user.userId);
+  }
+}
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class TodosService {}
+export class TodosService {
+  constructor(private usersService: UsersService) {}
+
+  async getAllTodos(userId: string): Promise<string[]> {
+    const user = await this.usersService.getUserByUserId(userId);
+    return user.todos;
+  }
+}
